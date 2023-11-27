d69deea0f4312961ea750ab7515a397a31a5aad4
Iryna <irasokolova25@ukr.net>
2023-11-27 20:31:57 +0200

    finish

---
+++ index.js
@@ -0,0 +1,33 @@
+const userName = 'Ira'; 
+const age = '25'+5;//255
+const productPrice = 20;
+const quantity = 2.5;
+
+let totalPrice = null;
+console.log(typeof quantity);
+
+totalPrice = productPrice * quantity;//40
+
+
+// console.log(quantity);
+// console.log('my first js code is: ', myCode);
+
+// Примітивні типи даних
+/**
+ * string
+ * number
+ * undefined
+ * null
+ * boolean - true / false
+ */
+const myCode = 'Awesome';
+
+console.log('7');
+console.log(typeof myCode);
+console.log(myCode.length);
+
+const isOnline = true;
+const isOpen = false;
+console.log(typeof isOnline);
+
+
