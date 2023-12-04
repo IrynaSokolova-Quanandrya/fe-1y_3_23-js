91390683977693d5798783317aa1a8bd5645080e
Iryna <irasokolova25@ukr.net>
2023-12-04 16:39:26 +0200

    підготувала матеріали

---
+++ js/01-numbers.js
@@ -0,0 +1,63 @@
+/*
+ * Парс числа з Number.parseInt() та Number.parseFloat()
+ */
+
+let elementWidth = '50px';
+
+
+let elementHeight = '200.74px';
+
+/*
+ * - Метод число.toFixed(digits)
+ * - Матрьошки на пркладі console.log(Number(число.toFixed(digits)))
+ */
+let salary = 1300.16472;
+
+
+/*
+ * - Приведення до числа з Number(value)
+ * - Значення NaN (Not a Number) і метод Number.isNaN(value)
+ */
+let quantity = '30';
+let value = 'Цей рядок неможливо перетворити в число';
+
+
+/*
+ * Об'ект Math
+ * - Возведення в степінь
+ * - Exponent operator **
+ */
+
+// const base = 2;
+// const power = 5;
+
+
+/*
+ * Напиши скрипт який просить у користувача ввести число і степінь,
+ * підводить число в цю степінь і виводить результат в консоль.
+ */
+
+// Алгоритм рішення:
+
+
+
+
+/*
+ * Генерим псевдовипадкові числа
+ * - Math.random()
+ * - Math.round()
+ */
+// Math.random() * (max - min) + min
+
+
+// const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+
+// const color = colors[index];
+
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
