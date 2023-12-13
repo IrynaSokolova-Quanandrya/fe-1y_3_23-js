11b7539c9c88c14dea0fbb5ea99fceab6e983462
Iryna <irasokolova25@ukr.net>
2023-12-13 19:50:17 +0200

    Update 02-comparison-operators.js

--- js/02-comparison-operators.js
+++ js/02-comparison-operators.js
@@ -3,7 +3,15 @@
  * Оператори порівняння
  */
 
-console.log(10 % 4);
+console.log(10 % 4);//2
+10 % 5 //0
+15 % 6 //3
+23 % 12 //11
+33 % 14 //5
+
+
+
+
 // console.log(15 > 3);
 
 /*
@@ -16,16 +24,31 @@ console.log(10 % 4);
  * Оператори рівності (== и ===) та нерівності (!= и !==)
  * ✅ «ЗАВЖДИ ВИКОРИСТОВУЙТЕ СУВОРУ РІВНІСТЬ (===) ТА НЕРІВНІСТЬ (!==)»
  */
+const num = 20;
+const str = '20';
 
+10 == 10 //true
+10 === 10 //true
 
-// console.log(5 === '5');
+20 == '20' //true
+num === str //false
 
+'' == false //true
+'' === false //false
 
+!false //true
+!true //false
 /**
  * 4. Що буде в консолі
-            const isEqual = 0 == false; 
+            const isEqual = 0 == false; //true
             const isEqual = 0 !== false; 
-            const isEqual = '12' == 12; 
-            const isEqual = '12' === 12; 
+            const isEqual = '12' == 12; //true
+            const isEqual = '12' === 12; //false
  */
 
+
+            !false //true
+            !true //false
+
+            2 === 4 //false
+            2 !== 4 //true
