442fbfb0e5c2a424edbd1a64df8696aa8fa0192d
Iryna <irasokolova25@ukr.net>
2023-12-25 20:39:56 +0200

    Update 01-switch.js

--- js/01-switch.js
+++ js/01-switch.js
@@ -1,13 +1,89 @@
+// Тернарний оператор
+const userName = 'Poly';
+let message;
+
+// if(userName === 'Poly'){
+//     message = 'Hello, Poly';
+// } else {
+//     message = 'Unknown userName'
+// }
+
+message = userName === 'Poly' ? 'Hello, Poly' : 'Unknown userName';
+
+// умова ? правдива умова : якщо умова неправдива
+
+// console.log(message);//'Hello, Poly'
+
+
+
+
 /*
  * Напиши скрипт вибору готеля по кількості зірок.
- * 1,2 - 20$,  3,4 - 50$,  5 - 120$
+ * 1,2 - 20$,  
+ * 3,4 - 50$,  
+ * 5 - 120$
  *
  * Якщо в змінній stars щось окрім чисел 1-5, виведи рядок
  * 'Токої кількості зірок немає'
  */
 
-const stars = 4;
+const stars = 1;
 let price;
+
+// if( stars === 1){
+//     price = 10
+//     console.log(`Ціна за номер в готелі ${price}$`);
+// } else if (stars === 2){
+//     price = 20
+//     console.log(`Ціна за номер в готелі ${price}$`);
+// } else if (stars === 3){
+//     price = 30
+//     console.log(`Ціна за номер в готелі ${price}$`);
+// } else if (stars === 4){
+//     price = 50
+//     console.log(`Ціна за номер в готелі ${price}$`);
+// } else if (stars === 5){
+//     price = 120
+//     console.log(`Ціна за номер в готелі ${price}$`);
+// } else {
+//     console.log('Токої кількості зірок немає');
+// }
+
+// switch (stars) {
+//     case 1:
+//     case 2: 
+//         price = 20
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break;
+//     case 3, 4: 
+//         price = 50
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break; 
+//     case 5: 
+//         price = 120
+//         console.log(`Ціна за номер в готелі ${price}$`);
+//         break;
+
+//     default: 
+//     console.log('Токої кількості зірок немає');
+//         break;
+// }
+
+const a = 5;
+
+if(true){
+    const b = 10;
+    const a = 15;
+    console.log(b);
+    console.log(a);
+}
+
+console.log(a);
+// console.log(b);
+
+
+
+
 
 
 
@@ -23,8 +99,8 @@ let price;
  */
 
 // 1 створюємо змінні
-const option = 5;
-let message = '';
+// const option = 5;
+// let message = '';
 
 // 2. робимо світч
 // 3. перевіряємо на рівність опцію і виводимо повідоилення
