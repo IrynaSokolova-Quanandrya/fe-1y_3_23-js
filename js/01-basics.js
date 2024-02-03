3569eb97b2ff5d4d1edb7d0477c916a343dbaaba
Iryna <irasokolova25@ukr.net>
2024-02-03 09:27:43 +0200

    розібрали

--- js/01-basics.js
+++ js/01-basics.js
@@ -8,16 +8,66 @@
  * - Оголошення функції (function declaration)
  */
 
-const fnA = function () {
+// const fnA = function () {
   
+// }
+// // console.log(fnB());
+
+// function fnB() {
+//   return 10;
+// }
+
+
+/**
+ * Посилальний тип даних
+ */
+// let number = 5;
+// const number2 = number;
+
+// number = 25;
+
+// console.log(number);
+// console.log(number2);
+
+// // console.log(number === number2);
+// // console.log('Hello' === 'Hello');
+
+// const arr1 = [];
+// const arr3 = arr1;
+
+// arr1.push(10)
+// console.log(arr1);
+// console.log(arr3);
+
+
+// console.log(arr1 === arr3);
+// console.log([1, 2, 3] === [1, 2, 3]);
+
+
+
+/**
+ * Callback function
+ */
+
+// const getUserName = function(){
+//   console.log(`Hello, Mary`);
+// }
+
+// const logName = function(callback){
+//   callback();
+// }
+
+// logName(getUserName)
+
+
+const fnA = function () {}
+function name() {}
+
+const fnB = (name)=>{
+  console.log(`Hi, ${name}!`);
 }
-console.log(fnB());
 
-function fnB() {
-  return 10;
-}
-
-
+fnB('Poly')
 
 
  /*
