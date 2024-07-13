93aecb3cabe4e4563498b77138fbf883a6fd20f0
Iryna <irasokolova25@ukr.net>
2024-07-13 09:52:01 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 // import './css/common.css';
+import castomer from "./js/castomer.json";
+import book from './js/castomer.json'
+
+
+const bookParsed = JSON.parse(book)
+console.log(bookParsed);
 
 const user = {
     name: 'Poly',
     age: 18,
     email: 'poly@ukr.net'
 }
+const userStr = JSON.stringify(user)
 
-console.log(user);
+console.log('userStr:', userStr);
+
+const userParsed = JSON.parse(userStr);
+
+console.log('userParsed:', userParsed);
+
+const str = 'Hello,world!'
+
+console.log(JSON.parse(str));
 
 
-const userJson = JSON.stringify(user)
-console.log(userJson);
 
-const user2 = JSON.parse(userJson)
 
-console.log(user2);
+
+
+
+
+
