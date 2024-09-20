99baa25e99af82efae9b314aec352c2dcf0d33d9
Iryna <irasokolova25@ukr.net>
2024-09-20 22:12:29 +0300

    додали пагінацію

--- src/index.js
+++ src/index.js
@@ -1,33 +1,50 @@
-/**
- * <li>
-    <a href="" target="_blank" rel="noopener noreferrer">
-      <article>
-        <img src="" alt="" width="480">
-        <h2></h2>
-        <p>Posted by: </p>
-        <p></p>
-      </article>
-    </a>
-  </li>
- */
-
 const formRef = document.querySelector('.js-search-form');
 const articlesContainerRef = document.querySelector('.js-articles-container');
+const loadMoreBtnRef = document.querySelector('[data-action="load-more"]') 
 
-const BASE_URL = 'https://newsapi.org/v2/everything/'
-const API_KEY = '3ce63eea477043d7a470d2b21dc5ab4b'
+formRef.addEventListener('submit', onSearch)
+loadMoreBtnRef.addEventListener('click', onLoadMore)
 
-const searchArticles = query => {
-    return fetch(`${BASE_URL}?q=${query}&apiKey=${API_KEY}`)
-    .then(response => response.json())
+const BASE_URL = 'https://newsapi.org/v2/everything/';
+const API_KEY = '3ce63eea477043d7a470d2b21dc5ab4b';
+
+const options = {
+    headers: {
+        'Authorization': API_KEY
+    }
 }
 
-searchArticles('flower')
+let page = 1;
+let searchQuery;
+
+const searchArticles = query => {
+    return fetch(`${BASE_URL}?q=${query}&pageSize=20&page=${page}`, options)
+        .then(response => response.json())    
+}
+function onSearch(e) {
+    e.preventDefault()
+    const form = e.currentTarget;    
+    searchQuery = form.elements.query.value;
+
+    searchArticles(searchQuery)
     .then(res => res.articles)
     .then(articles => {
         const markup = createArticleCards(articles)
-        articlesContainerRef.innerHTML = markup
+        articlesContainerRef.insertAdjacentHTML('beforeend', markup) 
+        page += 1;    
     })
+    form.reset()
+}
+
+function onLoadMore() {
+    searchArticles(searchQuery)
+    .then(res => res.articles)
+    .then(articles => {
+        const markup = createArticleCards(articles)
+        articlesContainerRef.insertAdjacentHTML('beforeend', markup) 
+         page += 1;
+    })
+}  
 
 function createArticleCards(articles) {
     return articles.map(article => {
