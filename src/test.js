06d0eafffa8d80429dab77cbe991570dc6448940
Iryna <irasokolova25@ukr.net>
2024-09-23 12:45:25 +0300

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,42 @@
+/**
+ * 1. В html існує елемент h1, створіть функцію яка приймає ім'я користувача та
+ * виводить його як заголовок. Перепишіть функцію всіма відомими вам способами. 
+ * 2. Створіть форму з полем email та кнопкою за допомогою шаблонного рядка та
+ * додайте її в DOM. Під час сабміту форми вивести повідомлення "Дякую за
+ * підписку", яке буде зникати через 1500мс
+ * 3. У вам є розмітка в html
+ * <div class="holder">
+  <h1>Title</h1>
+  <ul class="list">
+    <li>
+      <a href="#">link1</a>
+      <ul>
+        <li>
+          <a href="#">link2</a>
+        </li>
+        <li>
+          <a href="#">link3</a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="https://www.youtube.com/">link4</a>
+    </li>
+    <li>
+      <a href="https://validator.w3.org/">link5</a>
+    </li>
+    <li>
+      <a href="#">link6</a>
+    </li>
+  </ul>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, accusamus.
+  </p>
+  <h1>Title</h1>
+  <p>Lorem ipsum dolor sit amet.</p>
+</div>
+- зверніться через JS тільки до тих посилань, значення атрибутів href яких починається з https та зробіть їх червоного кольору;
+- зверніться через JS до <p>, який іде після <ul> і зробіть розмір його шрифта 24рх.
+ * 4. 
+ *
+ */
