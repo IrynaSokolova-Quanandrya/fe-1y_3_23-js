faeeff5ed97da21a0413d69a42d431cabf747192
Iryna <irasokolova25@ukr.net>
2024-10-17 15:17:32 +0300

    add tasks

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * 1. Створіть функцію всіма способами які ви знаєте?
+ * 2. Що буде в консолі
+ *                   getName('Rihanna');
+
+                function getName(name) {
+                    console.log(name);
+                }
+
+                getName('Sigma');
+
+                const getName = function (name) {
+                    console.log(name);
+                }
+ * 
+ */
+
+
