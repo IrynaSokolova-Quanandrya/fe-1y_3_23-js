6cec77a983c83b6c55485545232645378ab69ec2
Iryna <irasokolova25@ukr.net>
2024-11-21 17:05:09 +0200

    Update u - patch.js

--- src/js/u - patch.js
+++ src/js/u - patch.js
@@ -1,30 +1,25 @@
 const BASE_URL = 'http://localhost:3000';
 
 
+async function updateBookById(update, bookId) {
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(update),
+  };
+
+  const result = await fetch(`${BASE_URL}/books/${bookId}`, options)
+  const book = await result.json() 
+  
+    return book
+}
 
 
 
 
 
-
-
-
-
-
-
-
-
-// function updateBookById(update, bookId) {
-//   const options = {
-//     method: 'PATCH',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(update),
-//   };
-
-//   return fetch(`${BASE_URL}/books/${bookId}`, options).then(res => res.json());
-// }
 
 // updateBookById({ title: 'Велика нова книга по NODEJS' }, 19);
 
